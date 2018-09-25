aec02d74d4642cf373ad744044bfe969fac5f92b
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2018-09-25 15:11:33 -0500

    Add code to detect a spare

--- bowling/score.py
+++ bowling/score.py
@@ -2,5 +2,8 @@ def score_throw(throw: str) -> int:
   if throw == 'X':
     return 10
 
+  if throw == '/':
+    return 10
+
   else:
     return int(throw)
--- tests/score_test.py
+++ tests/score_test.py
@@ -5,6 +5,9 @@ class ScoringTest(TestCase):
   def test_x_is_ten(self):
     self.assertEqual(score_throw('X'), 10)
 
+  def test_spare_is_ten(self):
+    self.assertEqual(score_throw('/'), 10)
+
   def test_other_is_value(self):
     self.assertEqual(score_throw('9'), 9)
     self.assertEqual(score_throw('5'), 5)
