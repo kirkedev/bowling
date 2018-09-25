e65b10c3d99d3a57d413d1e5a1a8594a4fd63146
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2018-09-25 15:14:06 -0500

    Add code to detect a miss

--- bowling/score.py
+++ bowling/score.py
@@ -5,5 +5,8 @@ def score_throw(throw: str) -> int:
   elif throw == '/':
     return 10
 
+  elif throw == '-':
+    return 0
+
   else:
     return int(throw)
--- tests/score_test.py
+++ tests/score_test.py
@@ -8,6 +8,9 @@ class ScoringTest(TestCase):
   def test_spare_is_ten(self):
     self.assertEqual(score_throw('/'), 10)
 
+  def test_miss_is_zero(self):
+    self.assertEqual(score_throw('-'), 0)
+
   def test_other_is_value(self):
     self.assertEqual(score_throw('9'), 9)
     self.assertEqual(score_throw('5'), 5)
