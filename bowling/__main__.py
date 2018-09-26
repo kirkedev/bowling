0636e174e8e3b6f33197c0270973fbb903bf8ff9
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2018-09-26 14:26:29 -0500

    Improve main script help message

--- bowling/__main__.py
+++ bowling/__main__.py
@@ -2,6 +2,8 @@ import sys
 from bowling.score import score_game
 
 if len(sys.argv) < 2:
-  exit("Enter a string of bowling throws to score. Example: bin/bowling XXXXXXXXXXXX")
+  print("Enter a string of bowling throws to score, where 'X' is a strike, '/' is a spare, and '-' is a gutter.")
+  print("Example: bin/bowling XXXXXXXXXXXX")
+  exit(1)
 
 print(list(score_game(sys.argv[1])))
