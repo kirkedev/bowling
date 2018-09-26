aa7c18aab19d5e331c07d8c933de43affc66df31
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2018-09-26 13:14:16 -0500

    Reorganize

--- bowling/score.py
+++ bowling/score.py
@@ -1,6 +1,12 @@
 from itertools import accumulate
 from typing import Iterator, Tuple, List
 
+def is_spare(frame: str) -> int:
+  return frame[1] == '/'
+
+def is_strike(frame: str) -> int:
+  return frame[0] == 'X'
+
 def score_throw(throw: str) -> int:
   if throw in ('X', '/'):
     return 10
@@ -11,11 +17,8 @@ def score_throw(throw: str) -> int:
   else:
     return int(throw)
 
-def is_spare(frame: str) -> int:
-  return frame[1] == '/'
-
-def is_strike(frame: str) -> int:
-  return frame[0] == 'X'
+def score_throws(throws: str) -> int:
+  return sum(map(score_throw, throws))
 
 def split_frames(throws: str) -> Iterator[str]:
   frame_number = 1
@@ -44,14 +47,11 @@ def split_frames(throws: str) -> Iterator[str]:
       frame_number += 1
       i += 1
 
-def score_throws(throws: str) -> int:
-  return sum(map(score_throw, throws))
-
 def score_frame(frame: str) -> int:
   if is_strike(frame):
     return 10 + score_throws(frame[1:])
 
-  if is_spare(frame):
+  elif is_spare(frame):
     return 10 + score_throws(frame[2])
 
   else:
